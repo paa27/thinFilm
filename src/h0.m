682c85d603404f4d4009f824f39d9ff8d4b7cbf8
Pablo Andreas Arrutia Sota <paa27@njit.edu>
2018-11-28 14:51:41 -0500

    implemented pdepe

--- src/h0.m
+++ src/h0.m
@@ -1,1 +1,34 @@
 %ho solver
+m=0;
+x = linspace(0,10,10000);
+t = [0 0.005 0.01 0.05 0.1 0.5 1 1.5 2];
+
+sol = pdepe(m,@pdefun,@icfun,@bcfun,x,t);
+
+for i=1:length(t)
+    hold on
+    plot(x,sol(i,:,2))
+end 
+
+function [c,f,s] = pdefun(x,t,u,DuDx)
+D=5;
+c=[0;1];
+f=[-1 1;-u(2).^3 u(2).^3*D]*DuDx+[0;-u(2).^3];
+s=[0;0];
+end
+
+function u0 = icfun(x)
+b=0.1;
+xend=10;
+u0 = [-(1-b)/(2*tanh(xend/2))*2.*(sech(-(x-xend/2))).^2*tanh(-(x-xend/2));
+    (1-b)/(2*tanh(xend/2))*tanh(-(x-xend/2))+(1+b)/2];
+end
+
+function [pl,ql,pr,qr] = bcfun(xl,ul,xr,ur,t)
+b=0.1;
+pl = [0;1];
+ql = [1;1];
+pr = [0;b.^3];
+qr =[1;1];
+end
+

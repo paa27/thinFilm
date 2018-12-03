875a3b4d17daeea6f85c4d8b8f90a0a27c862728
Pablo Andreas Arrutia Sota <paa27@njit.edu>
2018-12-03 12:41:23 -0500

    globalized variables

--- src/h0.m
+++ src/h0.m
@@ -1,9 +1,15 @@
 %ho solver
+clear all;
 m=0;
-x = linspace(0,10,1000);
-t = linspace(0,2,10);
+global b;
+b = .1;
+global xend;
+xend = 20;
 
-%sol = pdepe(m,@pdefun,@icfun,@bcfun,x,t);
+x = linspace(0,xend,400);
+t = linspace(0,10,10);
+
+sol = pdepe(m,@pdefun,@icfun,@bcfun,x,t);
 
 for i=1:length(t)
     hold on
@@ -18,17 +24,16 @@ s=[-1;0].*u;
 end
 
 function u0 = icfun(x)
-b=0.1;
-xend=10;
+global b;
+global xend;
 u0 = [-(1-b)/(2*tanh(xend/2))*2.*(sech(-(x-xend/2))).^2*tanh(-(x-xend/2));
     (1-b)/(2*tanh(xend/2))*tanh(-(x-xend/2))+(1+b)/2];
 end
 
 function [pl,ql,pr,qr] = bcfun(xl,ul,xr,ur,t)
-b=0.1;
+global b;
 pl = [0;1];
 ql = [1;1];
 pr = [0;b.^3];
 qr =[1;1];
 end
-

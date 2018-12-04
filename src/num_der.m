efea0105ae1b337167e6fb1a26932740720a93a6
davyou <dy76@njit.edu>
2018-12-03 19:52:19 -0500

    num_der now returns the first and third derivative vectors

--- src/num_der.m
+++ src/num_der.m
@@ -1,15 +1,19 @@
 %this fucntion needs the x vector, solution vector, h spacing of whole
 %grid, and b
-function f = num_der(x,y,h,b)
+function [f,f3] = num_der(x,y,h,b)
 %expand the vector with ones and the b values at the end (used 100 ghost points on each side)
 o = ones(1,100);
 bs = b*ones(1,100);
 y = [o y(1:end) bs ];
 %calculate the derivative using the gradient function and the spacing h
 dydx = gradient(y,h);
+dydx2 = gradient(dydx,h);
+dydx3 = gradient(dydx2,h);
 %resize vector back to orginal size
 dydx = dydx(101:end-100);
+dydx3= dydx3(101:end-100);
 %plot(x,dydx)
 %the derivative vector is passed back through the function in f
 f = dydx;
+f3 = dydx3;
 end
